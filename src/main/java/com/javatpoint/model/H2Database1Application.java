6993e64bf42b03c98acf2c0dfd324a1cafc6d913
Mozan123 <moznyousif@gmail.com>
2022-06-08 23:00:09 -0400

    first commit

---
+++ src/main/java/com/javatpoint/model/H2Database1Application.java
@@ -0,0 +1,14 @@
+package com.javatpoint.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2Database1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2Database1Application.class, args);
+		
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Student(id, name, age, email) VALUES (1, 'Mozan', 26, "moznyousif@gmail.com");
+INSERT INTO Student(id, name, age, email) VALUES (2, 'Ali', 50, "ali123@gmail.com");
+INSERT INTO Student(id, name, age, email) VALUES (3, 'Ahmed', 39, "a.alsalih2@gmail.com");
